39203b5fee842a43371bdd55d9a9615d11bfef20
PowerEngine98 <ma.acosta13@uniandes.edu.co>
2020-11-03 12:52:21 -0500

    minor changes to user endpoint

--- routes/api.js
+++ routes/api.js
@@ -27,7 +27,20 @@ let headers = {
     'Content-Type': 'application/json'
 }
 
-router.get('/network/:username/', async (req, res) => {
+router.get('/user/:username/', async (req, res) => {
+    try {
+        let username = req.params.username
+        if (!username) {
+            throw new ApiError(ErrorCode.BAD_REQUEST, 'invalid username')
+        }
+        let user = await getUser(username)
+        res.json(user ? user : {})
+    } catch (error) {
+        res.status(error instanceof ApiError ? error.errorCode : ErrorCode.INTERNAL_SERVER_ERROR).json({ error: error.message })
+    }
+})
+
+router.post('/organizations/:username/', async (req, res) => {
     try {
         let username = req.params.username
         if (!username) {
@@ -35,7 +48,7 @@ router.get('/network/:username/', async (req, res) => {
         }
         let limit = req.query.limit
         if (!limit) {
-            limit = 20
+            limit = 15
         }
         if (!Number.isInteger(Number(limit))) {
             throw new ApiError(ErrorCode.BAD_REQUEST, 'invalid limit')
@@ -47,50 +60,44 @@ router.get('/network/:username/', async (req, res) => {
         if (!Number.isInteger(Number(limit))) {
             throw new ApiError(ErrorCode.BAD_REQUEST, 'invalid limit_per_organization')
         }
-        res.json(await getNetwork(username, limit, limit_per_organization))
+        let body = req.body
+        if (!body) {
+            throw new ApiError(ErrorCode.BAD_REQUEST, 'missing payload')
+        }
+        const { users, organizations } = body
+        if (!organizations || !Array.isArray(organizations)) {
+            throw new ApiError(ErrorCode.BAD_REQUEST, 'invalid organizations array')
+        }
+        if (!users || !Array.isArray(users)) {
+            throw new ApiError(ErrorCode.BAD_REQUEST, 'invalid users array')
+        }
+        res.json(await getOrganizations(username, organizations, users, limit, limit_per_organization))
     } catch (error) {
         res.status(error instanceof ApiError ? error.errorCode : ErrorCode.INTERNAL_SERVER_ERROR).json({ error: error.message })
     }
 })
 
-async function getNetwork(username, limit, limit_per_organization) {
-
-    const users = []
-    const organizations = []
-
-    async function getUser(username) {
-        let user_bio = await fetch(bio_profile_url + username).then(res => res.json())
-        let user = {
-            name: user_bio.person.name,
-            username: user_bio.person.publicId,
-            photo: user_bio.person.pictureThumbnail
-        }
-        //Flatten the job organizations where the user had infuence, removing repeated organizations
-        let organization_names = user_bio.experiences
-            .filter(experience => experience.category == 'jobs')
-            .reduce((organizations, experience) => organizations.concat(experience.organizations.map(organization => organization.name)), [])
-            .filter((organization_name, index, array) => array.indexOf(organization_name) == index)
-
-        user.organizations = organization_names
-        users.push(username)
-        return user
+async function getUser(username) {
+    let user_bio = await fetch(bio_profile_url + username).then(res => res.json())
+    if (!user_bio.person) {
+        return undefined
     }
-
-    async function getOrganizations(organization_names) {
-        let user_organizations = []
-        organization_names = organization_names.filter(organization_name => !organizations.includes(organization_name))
-        for (let organization_name of organization_names) {
-            organizations.push(organization_name)
-            let organization = {
-                name: organization_name,
-                members: users.length >= limit ? [] : await getOrganizationMembers(organization_name)
-            }
-            user_organizations.push(organization)
-        }
-        return Promise.all(user_organizations)
+    let user = {
+        name: user_bio.person.name,
+        username: user_bio.person.publicId,
+        weight: user_bio.person.weight,
+        photo: user_bio.person.pictureThumbnail,
+        organizations: []
     }
+    return user
+}
 
+async function getOrganizations(username, organizations, users, limit, limit_per_organization) {
+    let total = 0
     async function getOrganizationMembers(organization_name) {
+        if (total >= limit) {
+            return []
+        }
         let options = {
             organization: {
                 term: organization_name
@@ -101,36 +108,41 @@ async function getNetwork(username, limit, limit_per_organization) {
             body: JSON.stringify(options),
             headers: headers
         }).then(res => res.json())
-
         let members = []
         if (search_result.results) {
             let results = search_result.results
                 .filter(result => !users.includes(result.username))
                 .sort((r1, r2) => r2.weight - r1.weight)
+                .slice(0, limit_per_organization)
             for (let result of results) {
-                if (users.length >= limit) {
-                    break
-                } 
-                members.push(await getUser(result.username))
-                if (members.length >= limit_per_organization) {
+                users.push(result.username)
+                members.push(getUser(result.username))
+                total++
+                if (total >= limit) {
                     break
                 }
-            }
-            for(let user of members) {
-                user.organizations = await getOrganizations(user.organizations)
-                user.organizations = user.organizations.filter(organization => organization.members.length > 0)
-                if (users.length >= limit) {
-                    break
-                } 
             }
         }
         return Promise.all(members)
     }
 
-    let user = await getUser(username)
-    user.organizations = await getOrganizations(user.organizations)
-    user.organizations = user.organizations.filter(organization => organization.members.length > 0)
-    return user
+    let user_bio = await fetch(bio_profile_url + username).then(res => res.json())
+    //Flatten the job organizations where the user had infuence, removing repeated organizations
+    let organization_names = user_bio.experiences
+        .filter(experience => experience.category == 'jobs')
+        .reduce((organizations, experience) => organizations.concat(experience.organizations.map(organization => organization.name)), [])
+        .filter((organization_name, index, array) => array.indexOf(organization_name) == index)
+        .filter(organization_name => !organizations.includes(organization_name))
+
+    let user_organizations = []
+    for (let organization_name of organization_names) {
+        let organization = {
+            name: organization_name,
+            members: await getOrganizationMembers(organization_name)
+        }
+        user_organizations.push(organization)
+    }
+    return Promise.all(user_organizations)
 }
 
 module.exports = router
